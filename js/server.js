0ebbc6c3d5c39678df6c4aef959d06431c6fb751
sharma54 <91731467+sharma54@users.noreply.github.com>
2022-10-04 00:31:42 +0530

    Add files via upload

---
+++ js/server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRouter = require('./routes/userRouter');
+const noteRouter = require('./routes/noteRouter');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/users', userRouter);
+app.use('/api/notes', noteRouter);
+
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDb');
+});
+
+
+// below mongodb and above listen server
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+
+// Listen Server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port:', PORT);
+});
